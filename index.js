5a30e543cc2ef5ca971228649fead5eec9629185
Chris De Leon <cdeleon.ca91@gmail.com>
2021-08-10 16:07:16 -0400

    added routes to connect to front end

---
+++ index.js
@@ -0,0 +1,51 @@
+require('dotenv/config')
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose
+.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false,
+})
+.then(() => {
+    app.listen('5000', () => {
+        console.log('app is listening')
+    })
+})
+.catch(err => console.log(err))
+
+const UserSchema = mongoose.Schema({
+    fname: { type: String, required: true },
+    lname: { type: String, required: true },
+    lname: { type: String, required: true },
+    password: { type: String, required: true },
+},
+{ timestamp: true }
+)
+
+const UsersModel = mongoose.model('Users', UserSchema)
+
+app.get('/users', (req, res) => {
+    UsersModel.find()
+    .then(allUsers => res.status(200).send(allUsers))
+    .catch(err => console.log(err))
+})
+
+app.post('/users', (req, res) => {
+    console.log('req body', req.body)
+    new UsersModel(req.body)
+    .save()
+    .then(() => res.status(200).send('user has been created'))
+    .catch(err => console.log(err))
+})
+
+app.post('/login', (req, res) => {
+    UsersModel.findOne({email: req.body.email})
+    .then(userFound => res.status(200).send(userFound))
+    .catch(err => console.log(err))
+})
+
